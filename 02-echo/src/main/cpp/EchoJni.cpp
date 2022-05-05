b77220966217f95c903da917381862f64bac88d1
Man Nguyen <nmman37@gmail.com>
2022-05-05 19:42:29 +0700

    02-echo: implemented native methods

--- 02-echo/src/main/cpp/EchoJni.cpp
+++ 02-echo/src/main/cpp/EchoJni.cpp
@@ -4,44 +4,97 @@
 #define ECHO_CLASS          "jniexamples/echo/Echo"
 #define JAVAOBJECT_CLASS    "jniexamples/echo/JavaObject"
 
+static jstring getJavaObjectName(JNIEnv *env, jobject obj)
+{
+    jclass javaObjClazz = env->FindClass(JAVAOBJECT_CLASS);
+    jmethodID methodId = env->GetMethodID(javaObjClazz, "getName", "()Ljava/lang/String;");
+
+    return (jstring)env->CallObjectMethod(obj, methodId);
+}
+
 static void sendInteger(JNIEnv *env, jclass clazz, jint n1, jshort n2, jlong n3)
 {
-
+    std::cout << "Received integer: " << n1 << std::endl;
+    std::cout << "Received short: " << n2 << std::endl;
+    std::cout << "Received long: " << n3 << std::endl;
 }
 
 static void sendDecimal(JNIEnv *env, jclass clazz, jfloat f1, jdouble f2)
 {
-
+    std::cout << "Received float: " << f1 << std::endl;
+    std::cout << "Received double: " << f2 << std::endl;
 }
 
 static void sendChar(JNIEnv *env, jclass clazz, jchar ch)
 {
-
+    std::cout << "Received char: \'" << (char)ch << "\"\n";
 }
 
 static void sendString(JNIEnv *env, jclass clazz, jstring str)
 {
-
+    jboolean isCopy;
+    const char *chars = env->GetStringUTFChars(str, &isCopy);
+    std::cout << "Received string: \"" << chars << "\"\n";
+    env->ReleaseStringUTFChars(str, chars);
 }
 
 static void sendBoolean(JNIEnv *env, jclass clazz, jboolean b)
 {
-
+    std::cout << "Received boolean: " << (b == JNI_TRUE ? "true" : "false") << std::endl;
 }
 
 static void sendIntegerArray(JNIEnv *env, jclass clazz, jintArray arr)
 {
+    jboolean isCopy;
+    jsize sz = env->GetArrayLength(arr);
+    jint *arr_p = env->GetIntArrayElements(arr, &isCopy);
 
+    if (arr_p == NULL) {
+        return;
+    }
+
+    if (sz > 0) {
+        std::cout << "Received integer array: [";
+        for (jsize i = 0; i < sz - 1; ++i) {
+            std::cout << arr_p[i] << ", ";
+        }
+        std::cout << arr_p[sz-1] << "]\n";
+    }
+
+    env->ReleaseIntArrayElements(arr, arr_p, JNI_ABORT);
 }
 
 static void sendObject(JNIEnv *env, jclass clazz, jobject obj)
-{
+{   
+    jboolean isCopy;
+    jstring name = getJavaObjectName(env, obj);
 
+    const char *chars = env->GetStringUTFChars(name, &isCopy);
+    std::cout << "Received object: Object{\"" << chars << "\"}" << std::endl;
+    env->ReleaseStringUTFChars(name, chars);
 }
 
 static void sendObjectArray(JNIEnv *env, jclass clazz, jobjectArray objArr)
 {
+    jsize sz = env->GetArrayLength(objArr);
 
+    auto print = [env](jobject o) {
+        jboolean isCopy;
+        jstring name = getJavaObjectName(env, o);
+        const char *chars = env->GetStringUTFChars(name, &isCopy);
+        std::cout << "Object{\"" << chars << "\"}";
+        env->ReleaseStringUTFChars(name, chars);
+    };
+
+    if (sz > 0) {
+        std::cout << "Received object array: [";
+        for (jsize i = 0; i < sz - 1; ++i) {
+            print(env->GetObjectArrayElement(objArr, i));
+            std::cout << ", ";
+        }
+        print(env->GetObjectArrayElement(objArr, sz - 1));
+        std::cout << "]\n";
+    }
 }
 
 static const JNINativeMethod methods[] = {
